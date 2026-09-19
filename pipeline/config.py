fe6de66735d81e62f0de3f1d34cc0a65219c490a
cofferic7 <ericywj06@gmail.com>
2026-09-19 19:26:17 +0800

    Modifying config files because it doesn't have anything

---
+++ pipeline/config.py
@@ -0,0 +1,4 @@
+FIELDS = ["shipper", "consignee", "notify_party", "port_of_loading",
+          "port_of_discharge", "container_count", "gross_weight_kg"]
+CATEGORIES = ["BL_COMPARISON", "SI_REQUEST", "INVOICE_QUERY", "GENERAL", "SPAM"]
+REVIEW_REASONS = ["wrong_doc_type", "missing_attachment", "unreadable", "missing_value"]
