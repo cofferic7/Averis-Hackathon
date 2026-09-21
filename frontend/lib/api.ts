1b76c8361d95fab4e4884a0259e1c3b15f10c16c
cofferic7 <ericywj06@gmail.com>
2026-09-21 22:37:46 +0800

    Changes the api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
-
-   export async function getResults() {
+export async function getResults() {
        const response = await fetch("/report_data.json");
        if (!response.ok) {
            throw new Error("Failed to fetch results");
